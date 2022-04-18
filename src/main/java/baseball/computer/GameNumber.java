ab33e533ccdab23fb60e851fc4cb7077d57113a0
sangho lee <sangholee.dev@gmail.com>
2022-04-18 21:36:58 +0900

    feat: 숫자 테스트 추가

--- src/main/java/baseball/computer/GameNumber.java
+++ src/main/java/baseball/computer/GameNumber.java
@@ -1,8 +1,7 @@
 package baseball.computer;
 
-import baseball.game.Game;
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Arrays;
+import java.util.Objects;
 
 public class GameNumber {
     private final int number;
@@ -13,12 +12,36 @@ public class GameNumber {
     private GameNumber() {
         this.number = Randoms.pickNumberInRange(MIN, MAX);
     }
+    private GameNumber(int number) {
+        this.number = number;
+    }
 
     public static GameNumber generate() {
         return new GameNumber();
     }
 
+    public static GameNumber of(final int number) {
+        return new GameNumber(number);
+    }
+
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GameNumber that = (GameNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
